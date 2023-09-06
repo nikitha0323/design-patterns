a1e81f548b7e5b077ed62ecacf9f9b99742ecf33
Nikitha Kondapeta <nikitha@email.sc.edu>
2023-09-05 22:21:43 -0400

    adding new clases create

---
+++ Strategy/Player.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ * @author nikitha kondapeta
+ * Player class for football players
+ */
+public abstract class Player {
+
+	protected String name;
+	private boolean defence=true;
+	protected DefenceBehavior defenceBehavior;
+	protected OffenceBehavior offenceBehavior;
+	/**
+	 * Creates a player with an indiciated name and sets the Defence and offence Behavior
+	 * 
+	 */
+	public Player(String name)
+	{
+		this.name=name;
+		setDefenceBehavior();
+		setOffenceBehavior();
+	
+	}
+	public abstract void setDefenceBehavior();
+	
+	public abstract void setOffenceBehavior();
+	/**
+	 * 
+	 * String play is representated below
+	 */
+	
+	public String play() 
+	{
+		if(this.defence) 
+		{
+			return this.offenceBehavior.play();
+			
+        }
+		else 
+		{
+			if (this.defenceBehavior==null)
+				return "not playing";
+			else
+				return this.defenceBehavior.play();
+        }
+}
+	public void turnover()
+	{
+		this.defence=!this.defence;
+	}
+}
---
+++ Strategy/SackBehavior.java
@@ -0,0 +1,11 @@
+public class SackBehavior implements DefenceBehavior{
+	/**
+	 * Another statement when Sack Behavior is called for the defence behavior
+	 */
+	public String play()
+	{
+		return " Sack the quarterback";
+	}
+
+	
+}
